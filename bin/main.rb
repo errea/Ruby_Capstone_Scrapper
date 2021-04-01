d18572deb241dc6d6aafad70fd069da879f376fe
errea <erreakezy@gmail.com>
2021-04-01 17:20:57 +0100

    Modify main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,29 @@
-require_relative '../lib/scrapper'
 require_relative '../lib/input_check'
+require_relative '../lib/scrapper'
 
-@input_check = Inputcheck.new
+
 @ecom = Scrapper.new
+@input_check = Inputcheck.new
+
+def  show_ecom(product_name, product_price)
+    
+end
+
+def ecom_get_index
+  index = @ecommerce.user_menu_index
+  amount = @ecommerce.ecom_amount
+  @input_check.display_clear
+  puts '+***************************+'
+  puts 'Can you Please select an option?'
+  index[0].each_with_index { |item, i| puts "#{i + 1}. #{item} (#{amount[i]} ecommerce)"}
+  puts '+***************************+'
+  user_answer = @input_check.num_check(gets.chomp.to_i, 1, index.length + 1)
+  product_price = ''
+  product_name = ''
+  @ecommerce.user_menu_index[0].each_with_index {|item, index| name = item if index == (ans - 1)}
+  @ecommerce.user_menu_index[1].each_with_index {|item, index| link = item if index == (ans - 1)}
+  print_movies(link, product_name)
+end
 
 def user_menu #this is the main menu interface which the user would see
   @input_check.display_clear
@@ -16,7 +37,7 @@ def user_menu #this is the main menu interface which the user would see
   input = @input_check.num_check(gets.chomp.to_i, 1,4)
   case input
   when 1
-    ecom_index_index
+    ecom_get_index
   when 2
   when 3
   when 4
@@ -24,10 +45,6 @@ def user_menu #this is the main menu interface which the user would see
   end
 end
 
-def ecom_index_index
-  
-
-end
 
 def finish_scrapping    #when the user is done scrapper the site this method ends the process
   puts 'Good Bye!!! HAPPY product HAPPY customer'
@@ -35,10 +52,12 @@ end
 
 def welcome_screen_user
   @input_check.display_clear
+  puts '+***************************************+'
   puts 'Hello, You Are here!!!LOL, TAKE A TOUR'
   puts 'press any key to PROCEED'
+  puts '+***************************************+'
   gets
-  main_menu
+  user_menu
 end
 
 welcome_screen_user
