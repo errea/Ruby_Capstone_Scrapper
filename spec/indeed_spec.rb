4c2103d1cc347750591306f74b27b158cdb37f34
errea <erreakezy@gmail.com>
2021-04-02 00:52:40 +0100

    Add spec/indeed_spec.rb for testing indeed script

---
+++ spec/indeed_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'spec_helper'
+require_relative '../lib/scraper'
+require_relative '../lib/udacity_scraper'
+require_relative '../lib/indeed_scraper'
+require_relative '../lib/remoteio_scraper'
+
+RSpec.describe IndeedScraper do
+  let(:url) { 'https://www.indeed.com/jobs?q=Ruby+On+Rails&l=Remote&rbl=Remote&jlid=aaa2b906602aa8f5&sort=date' }
+
+  subject { IndeedScraper.new(url) }
+
+  it 'should create indeed_jobs.csv after invoking #scrap' do
+    puts "We're testing if website scrapping from indeed.com works fine..."
+    subject.scrap
+    expect(File.exist?('indeed_jobs.csv')).to be_truthy
+  end
+end
