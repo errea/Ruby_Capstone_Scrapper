4e5a869545bbd798fec1f18f64b287bd87176092
errea <erreakezy@gmail.com>
2021-04-01 12:17:47 +0100

    Add function to initialize and check the total pages in the site

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -5,34 +5,47 @@ require "open-uri"
 
 class Scrape
     def initialize
-
-        unparsed = HTTParty.get('https://w')
-        @parsed = Nokogiri::HTML(unparsed)
-    end 
-
-
-
-    url = "https://www.indeed.com/"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    job = Array.new
-    job_listings_pages = parsed_page.css('td#resultsCol') #data for all the jobs
-    set_page = 1
-    per_page = job_listings_pages.count
-   # total_pages = parsed_page.css('div#searchCountPages').text.split(' ')[1].gsub(',', '').to_i #22,365
-    #last_page = (total.to_f/)
-    job_listings_pages.each do | job_listings_page|  #iterating over our job listings
-        job = { 
-            title: job_listings_page.css('').text,
-            company: job_listings_page.css('span.company').text,
-            location: job_listings_page.css('span.location.accessible-contrast-color-location').text,
-            type: job_listings_page.css('span.remote').test,
-            description: job_listings_page.css('div.summary').text,
-            date: job_listings_page.css('span.date.date-ally').test
-         }
-         jobs << job
-    end
-    byebug
-end
-
-scrape
+            unparsed = HTTParty.get('https://en.wikipedia.org/wiki/Lists_of_films#Alphabetical_indices')
+            @parsed = Nokogiri::HTML(unparsed)
+          end
+        
+          def menu_index
+            @index = []
+            name = []
+            links = []
+            array = @parsed.css('table.wikitable').css('tr').css('td').css('a')
+            (array.length - 3).times do |i|
+              name.push(array[i].text)
+              links.push(array[i].attributes['href'].value)
+            end
+            @index.push(name)
+            @index.push(links)
+            @index
+          end
+        
+          def movies_amount
+            amount = []
+            @index[1].each_with_index do |item, i|
+              unparsed = HTTParty.get("")
+              parsed = Nokogiri::HTML(unparsed)
+              amount[i] = parsed.css('div.div-col').css('a').count
+            end
+            amount
+          end
+        
+          def menu_movies(input)
+            unparsed = HTTParty.get("https://en.wikipedia.org/#{input}")
+            parsed = Nokogiri::HTML(unparsed)
+            name = []
+            links = []
+            result = []
+            array = parsed.css('div.div-col').css('a')
+            array.length.times do |i|
+              name.push(array[i].text)
+              links.push(array[i].attributes['href'].value)
+            end
+            result.push(name)
+            result.push(links)
+            result
+          end
+        end
