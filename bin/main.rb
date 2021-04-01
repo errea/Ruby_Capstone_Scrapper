e39860cad40f2ec7d9d3b7e19c52f324ae26cf61
errea <erreakezy@gmail.com>
2021-04-01 14:29:53 +0100

    Add function ecom_index_index

--- bin/main.rb
+++ bin/main.rb
@@ -1,78 +1,41 @@
 require_relative '../lib/scrapper'
 require_relative '../lib/input_check'
 
-@input_check = Imputcheck.new
-#@movies = Scrapper.new
+@input_check = Inputcheck.new
+@ecom = Scrapper.new
 
 def user_menu #this is the main menu interface which the user would see
   @input_check.display_clear
   puts 'Menu'
-  puts '+---------------------+'
-  puts '1. Show index'
-  puts '2. Search options'
-  puts '3. All movies'
-  puts '4. Credits'
-  puts '5. Exit'
-  puts '+---------------------+'
-  input = @input_check.num_check(gets.chomp.to_i, 1,5)
+  puts '+***********************+'
+  puts '1. Show Products'
+  puts '2. All Ecommerce Products'
+  puts '3. Credits'
+  puts '4. Exit'
+  puts '+***********************+'
+  input = @input_check.num_check(gets.chomp.to_i, 1,4)
   case input
   when 1
-    movie_index_by
+    ecom_index_index
   when 2
   when 3
   when 4
-  when 5
     finish_scrapping
   end
 end
 
-def movie_index_by
-  index = @movies.menu_index
-  amount = @movies.movies_amount
-  @input_checker.display_clear
-  puts '+---------------------+'
-  puts 'Select an option'
-  index[0].each_with_index { |item, i| puts "#{i + 1}. #{item} (#{amount[i]} movies)"}
-  puts '+---------------------+'
-  ans = @input_checker.number_checker(gets.chomp.to_i, 1, index.length + 1)
-  link = ''
-  name = ''
-  @movies.menu_index[0].each_with_index {|item, index| name = item if index == (ans - 1)}
-  @movies.menu_index[1].each_with_index {|item, index| link = item if index == (ans - 1)}
-  print_movies(link, name)
-end
-
-def print_movies(link, name)
-  array = @movies.menu_movies(link)
-  i = 0
-  while i < array[0].length
-  @input_checker.display_clear
-  p array[0].length
-  puts '+-------------------------------+'
-  puts "All movies for #{name} from #{i + 1} to #{i + 20}"
-  array[0].each_with_index {|item, index| puts "#{index + 1}. #{item}" if index >= i && index <= (i + 19) }
-  puts '+-------------------------------+'
-  puts 'Press B for previous result of N for next:'
-  puts '+-------------------------------+'
-  ans = @input_checker.number_checker(gets.chomp, i + 1, i + 20)
+def ecom_index_index
   
-  if ans.upcase == "B"
-    i += 20 
-  elsif ans.upcase == 'N'
-    i -= 20
-  end
-
-  end
 
 end
 
-def finish_scrapping
-  puts 'Good Bye!!! HAPPY CODING'
+def finish_scrapping    #when the user is done scrapper the site this method ends the process
+  puts 'Good Bye!!! HAPPY product HAPPY customer'
 end
 
 def welcome_screen_user
   @input_check.display_clear
-  puts 'Hi, You Are here!!!LOL, TAKE A TOUR'
+  puts 'Hello, You Are here!!!LOL, TAKE A TOUR'
   puts 'press any key to PROCEED'
   gets
   main_menu
